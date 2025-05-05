38a6529e7ddb0481a83f9d10ed0f404da4dcbe88
Alexandre Foletto <alexandre.foletto@segfy.com>
2025-05-05 17:38:57 -0300

    Configure cypress-split

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require("cypress")
 
+const cypressSplit = require("cypress-split")
+
 module.exports = defineConfig({
   viewportHeight: 1024,
   viewportWidth: 1700,
@@ -9,5 +11,9 @@ module.exports = defineConfig({
         grepFilterSpecs: true,
       },
     fixturesFolder: false,
+    setupNodeEvents(on, config) {
+      cypressSplit(on, config)
+      return config
+    }
   },
 })
